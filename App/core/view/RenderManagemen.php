e98deb35fabb2c5548e8b9ab1ae77a81069b7ff8
Alireza Belal <belal.alireza@yahoo.com>
2021-09-02 02:37:51 -0700

    Manage subpages with the central page

---
+++ App/core/view/RenderManagemen.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\core\view;
+
+use App\core\Application;
+use App\core\Message;
+
+
+class RenderManagemen
+{
+    private static string $BASE_PATH = "layouts/main";
+
+
+    public static function renderView($view, $params = [])
+    {
+        return str_replace("{{content}}", self::loadView($view, $params), self::loadView(self::$BASE_PATH));
+    }
+
+
+    private static function loadView($view, $params = []) 
+    {
+        foreach ($params as $key => $value) 
+        {
+            $$key = $value;
+        }
+
+        if (Message::check())
+        {
+            $message = Message::getMessage();
+            $type = Message::getType();
+            Message::clear();
+        }
+
+        ob_start();
+        include_once Application::$ROOT . "/App/view/" . $view . ".php";
+        return ob_get_clean();
+    }
+}
