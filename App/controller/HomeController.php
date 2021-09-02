ab0811b26711a0ed0ebebd22be7bac862b2b282f
Alireza Belal <belal.alireza@yahoo.com>
2021-09-02 02:32:25 -0700

    Index page control

---
+++ App/controller/HomeController.php
@@ -0,0 +1,17 @@
+<?php 
+
+namespace App\controller;
+
+use App\model\File;
+use App\core\Auth;
+
+
+class HomeController extends BaseController
+{
+    public function index()
+    {
+        $files = (new File())->fetchTotal();
+        $files = json_encode($files);
+        return $this->render("home", ['files' => $files, 'isAdmin' => Auth::isUserAdmin()]);
+    }
+}
