1d51cd6ea77a85d028943975d717e29b95ccd839
Alireza Belal <belal.alireza@yahoo.com>
2021-09-02 02:31:06 -0700

    User registration

---
+++ App/controller/traitsController/RegisterTrait.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\controller\traitsController;
+
+use App\middleware\Validation;
+use App\core\Auth;
+use App\core\Message;
+use App\model\User;
+
+
+trait RegisterTrait
+{
+
+    public function register($request) {
+        $data = $request->getBody();
+
+        $checkValid = Validation::validate($data);
+
+        if ($checkValid) {
+            $user = (new User())->register($data['username'], $data['password']);
+            if ($user == -1) {
+                Message::addMessage("Registration was unsuccessful", Message::Error);
+                return $this->redirect("register", 303);
+            } else {
+                Message::addMessage("Registration completed successfully.", Message::Successful);
+                return $this->redirect("login");
+            }
+        } else {
+            return $this->redirect("register", 303);
+        }
+    }
+}
