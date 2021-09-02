36b2444675c5254cdd499426575c9a089d8ae4e3
Alireza Belal <belal.alireza@yahoo.com>
2021-09-02 02:42:46 -0700

    Manage the uploaded file

---
+++ App/middleware/FileControl.php
@@ -0,0 +1,37 @@
+<?php 
+
+namespace App\middleware;
+
+use App\core\Message;
+
+
+class FileControl
+{
+    const DATA_LIMIT = 52428800 ; //50MB
+    const DATA_TYPES = ['MKV', 'MP4', 'MOV', 'GIF' , 'PNG' , 'JPEG'];
+
+    public static function sizeCheck($data)
+    {
+        return $data > self::DATA_LIMIT;
+    }
+
+
+    public static function typeCheck($data)
+    {
+        return in_array(strtoupper($data), self::DATA_TYPES);
+    }
+
+
+    public static function validate($size, $type)
+    {
+        if (self::sizeCheck($size)) {
+            Message::addMessage("File is larger than allowed. Please select another file", Message::Error);
+            return false;
+        }
+        if (!self::typeCheck($type)) {
+            Message::addMessage("File should be " . implode(", ", self::DATA_TYPES) . " . $type", Message::Error);
+            return false;
+        }
+        return true;
+    }
+}
