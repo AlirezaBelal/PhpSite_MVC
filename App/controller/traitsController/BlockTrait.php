e200def72ec94e17abc22c9318495b5db38abd52
Alireza Belal <belal.alireza@yahoo.com>
2021-09-03 00:31:49 -0700

    add comont -- edit Method -- delet method unblock

--- App/controller/traitsController/BlockTrait.php
+++ App/controller/traitsController/BlockTrait.php
@@ -10,24 +10,25 @@ use App\core\Auth;
 //Block User and unblock User
 trait BlockTrait
 {
-
     public function block($request)
     {
         $data = $request->getBody();
+        $checkValid = Validation::toBeRight($data);
 
-        $checkValid = Validation::validate($data);
-
+        //Access check
         if (!Auth::isUserAdmin())
         {
             Message::addMessage("Access is blocked.");
             return $this->redirect("home", 303);
         }
 
+
+        //0 -> block
         if ($checkValid) {
-            if ((new User())->lockByName($data['name'], 0)) {
+            if ((new User())->blockByName($data['name'], 0)) {
                 Message::addMessage("blocking user completed successfully", Message::Wrong);
             } else {
-                Message::addMessage("Failed to blocking user", Message::ERROR);
+                Message::addMessage("Failed to blocking user", Message::Error);
             }
             return $this->redirect("users");
         } else {
@@ -35,29 +36,5 @@ trait BlockTrait
         }
     }
 
-
-    public function unblock($request)
-    {
-        $data = $request->getBody();
-
-        $checkValid = Validation::validate($data);
-
-        if (!Auth::isUserAdmin())
-        {
-            Message::addMessage("Access is blocked.");
-            return $this->redirect("home", 303);
-        }
-
-        if ($checkValid) {
-            if ((new User())->lockByName($data['name'], 1)) {
-                Message::addMessage("unblocking user completed successfully", Message::Successful);
-            } else {
-                Message::addMessage("Failed to unblocking user", Message::Error);
-            }
-            return $this->redirect("users");
-        } else {
-            return $this->redirect("users", 307);
-        }
-    }
 }
 
